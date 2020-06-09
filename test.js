5c635de6ac3632ad15d13525fd2c116bf6b64dad
Norc <64667579+Norc@users.noreply.github.com>
2020-06-09 05:58:18 -0400

    last update before ignore?

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ let str = '2d6 + 2 + 2d6 + 2'
 let rageDmg = '2';
 let patt = `\\s\\+\\s${rageDmg}($|[^0123456789dkrxcm(@{})])`;
 console.log(`${patt}`);
-//var patt = /\s\+\s2($|[^0123456789dkrxcm(@{})])/ig;
 let result = str.search(patt);
 console.log(result);
 if (result !== -1) {
